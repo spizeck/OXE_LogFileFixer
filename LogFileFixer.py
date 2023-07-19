299990ec7727b2dddbe3df5edfe7a524d939a43b
Chad Nuttall <chadnuttall1@gmail.com>
2023-07-18 21:39:51 -0400

    On branch main Your branch is up to date with 'origin/main'.
    Changes to be committed:
    	modified:   LogFileFixer.py
    
    Refactored to prep the code for the next step of the project.

--- LogFileFixer.py
+++ LogFileFixer.py
@@ -2,50 +2,78 @@ import pandas as pd
 import tkinter as tk
 import ttkbootstrap as ttkb
 from tkinter import filedialog, messagebox, ttk
+from datetime import datetime
 
 
-def load_file():
-    file_path = filedialog.askopenfilename()
-    df = pd.read_csv(file_path, dtype=str)
-    return df
+class CSVHandler:
+    @staticmethod
+    def load_file(file_path):
+        try:
+            return pd.read_csv(file_path, dtype=str)
+        except Exception as e:
+            messagebox.showerror('Error', f'Failed to load CSV file. Error: {str(e)}')
+            return None
+
+    @staticmethod
+    def save_file(df, headers, file_path):
+        try:
+            new_df = df[headers]
+            new_df.to_csv(file_path, index=False)
+        except Exception as e:
+            messagebox.showerror('Error', f'Failed to save CSV file. Error: {str(e)}')
+            return False
+
+        return True
 
 
-def select_headers(df):
-    style = ttkb.Style(theme='lumen')
-    root = style.master
-    root.title('OXE Log File Fixer by Chad')
-    root.geometry('500x500')
+    
+    
 
-    frame = ttk.Frame(root)
-    frame.pack(padx=10, pady=10)
+class App:
+    def __init__(self, root, df):
+        self.root = root
+        self.df = df
+        self.style = ttkb.Style(theme='lumen')
+        self.root.title('OXE Log File Fixer by Chad')
+        self.root.geometry('500x500')
 
-    def on_button_click():
-        selected_indices = listbox.curselection()
-        selected_headers = [listbox.get(i) for i in selected_indices]
-        new_df = df[selected_headers]
-        new_df.to_csv('new_file.csv', index=False)
-        messagebox.showinfo('Success', 'New CSV file has been created with selected headers.')
-        root.destroy()
+        frame = ttk.Frame(root)
+        frame.pack(padx=10, pady=10)
 
-    button = ttkb.Button(root, text='Create New CSV with Selected Columns', command=on_button_click)
-    button.pack(pady=15)
+        button = ttkb.Button(root, text='Create New CSV with Selected Columns', command=self.on_button_click)
+        button.pack(pady=15)
 
-    listbox = tk.Listbox(root, selectmode=tk.MULTIPLE, width=80)
-    listbox.pack(fill='both', side=tk.LEFT)
+        self.listbox = tk.Listbox(root, selectmode=tk.MULTIPLE, width=80)
+        self.listbox.pack(fill='both', side=tk.LEFT)
 
-    scrollbar = ttk.Scrollbar(root, orient='vertical', command=listbox.yview)
-    scrollbar.pack(fill='y', side=tk.RIGHT)
+        scrollbar = ttk.Scrollbar(root, orient='vertical', command=self.listbox.yview)
+        scrollbar.pack(fill='y', side=tk.RIGHT)
 
-    for column in df.columns:
-        listbox.insert(tk.END, column)
+        for column in self.df.columns:
+            self.listbox.insert(tk.END, column)
 
-    root.mainloop()
+    def on_button_click(self):
+        selected_indices = self.listbox.curselection()
+        selected_headers = [self.listbox.get(i) for i in selected_indices]
+
+        output_file_path = filedialog.asksaveasfilename(defaultextension=".csv", initialfile=f'new_file_{datetime.now().strftime("%Y%m%d%H%M%S")}')
+        if not output_file_path:
+            return
+
+        if CSVHandler.save_file(self.df, selected_headers, output_file_path):
+            messagebox.showinfo('Success', 'New CSV file has been created with selected headers.')
+            self.root.destroy()
 
 
 def main():
-    df = load_file()
-    select_headers(df)
+    file_path = filedialog.askopenfilename()
+    df = CSVHandler.load_file(file_path)
+    if df is not None:
+        root = tk.Tk()
+        app = App(root, df)
+        root.mainloop()
 
 
 if __name__ == "__main__":
     main()
+
